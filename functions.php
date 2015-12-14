c6e36fd1d42d83ce7c71d873f5879045617075d8
Chris Reynolds <chris@jazzsequence.com>
2015-12-14 11:05:24 -0700

    enqueue the main.js file

--- functions.php
+++ functions.php
@@ -113,6 +113,10 @@ add_action( 'widgets_init', 'exhibit_widgets_init' );
 function exhibit_scripts() {
 	wp_enqueue_style( 'exhibit-style', get_stylesheet_uri() );
 
+	$version = filemtime( get_template_directory() . '/js/main.js' );
+
+	wp_enqueue_script( 'wcus-main', esc_url( get_template_directory_uri() . '/js/main.js' ), array( 'jquery', 'underscore', 'director' ), $version, true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
